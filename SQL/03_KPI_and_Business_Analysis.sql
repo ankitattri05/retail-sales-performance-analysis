a7896ae5f4a847200833fedd3a4063d827558107
ankitattri05 <ankitattri05@gmail.com>
2026-07-15 00:32:08 +0530

    Fix KPI query and add average order value metric

--- SQL/03_KPI_and_Business_Analysis.sql
+++ SQL/03_KPI_and_Business_Analysis.sql
@@ -8,10 +8,10 @@
 SELECT
     ROUND(SUM(sales),2) AS total_sales,
     ROUND(SUM(profit),2) AS total_profit,
-    ROUND((SUM(profit)/SUM(sales))*100,2) AS profit_margin_pct,
+    ROUND((SUM(profit)/NULLIF(SUM(sales),0))*100,2) AS profit_margin_pct,
     COUNT(DISTINCT order_id) AS total_orders,
-    ROUND(SUM(sales)/COUNT(DISTINCT order_id),2
-
+    ROUND(SUM(sales)/COUNT(DISTINCT order_id),2) AS avg_order_value
+FROM store;
 -- Shipping cost contribution
 
 SELECT
